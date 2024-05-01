178b109542582809bde80c40d186c65caf73a6e7
Erik Simon <erik@es-dev.xyz>
2024-05-01 19:21:36 +0200

    Created Project

---
+++ Source/Application.cpp
@@ -0,0 +1,18 @@
+//
+// Created by erik on 01.05.24.
+//
+
+#include "Application.h"
+
+Application::Application() {}
+
+Application::~Application() {}
+
+void Application::Run() {
+}
+
+void Application::initWindow() {
+}
+
+void Application::initVulcan() {
+}
---
+++ Source/Application.h
@@ -0,0 +1,26 @@
+//
+// Created by erik on 01.05.24.
+//
+
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+
+
+class Application {
+
+public:
+    Application();
+    ~Application();
+
+    void Run();
+
+private:
+    void initWindow();
+    void initVulcan();
+
+};
+
+
+
+#endif //APPLICATION_H
